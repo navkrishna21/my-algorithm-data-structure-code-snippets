d6f674ab8e75b06c415293bc8f19eff3a7ec317a
Navkrishna <navkrishna21@github.com>
2019-10-07 03:31:38 +0530

    Added Modular Exponentiation

---
+++ modular_exponentiation/modular_exponentiation.cpp
@@ -0,0 +1,24 @@
+//recursive
+long long binpow(long long a, long long b) {
+    if (b == 0)
+        return 1;
+    long long res = binpow(a, b / 2);
+    if (b % 2)
+        return res * res * a;
+    else
+        return res * res;
+}
+
+
+//iterative
+template <typename T>
+T modpow(T base, T exp, T modulus) {
+  base %= modulus;
+  T result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = (result * base) % modulus;
+    base = (base * base) % modulus;
+    exp >>= 1;
+  }
+  return result;
+}
