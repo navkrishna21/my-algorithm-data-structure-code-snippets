936f321114cb987eda2d73af5edf8b00bcdc0b47
Navkrishna <navkrishna.raghav@gmail.com>
2019-10-07 04:14:20 +0530

    Update matrix_exponentiation.cpp

--- matrix_exponentiation/matrix_exponentiation.cpp
+++ matrix_exponentiation/matrix_exponentiation.cpp
@@ -1,9 +1,52 @@
 #include <bits/stdc++.h>
 #define MOD 1000000007
 using namespace std;
-typedef vector<vector<long long>> matrix;
+typedef vector<vector<long long>> matrix; //matrix is defined as vector of vector
  
-matrix mul(matrix a,matrix b){
+//function declarations for matrix multiplication, addition, subtraction and, identity matrix
+matrix mul(const matrix &,const matrix &);
+matrix add(const matrix &,const matrix &);
+matrix sub(const matrix &,const matrix &);
+matrix identity_matrix(long long);
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long m,n;
+        cin>>m>>n;
+ 
+        matrix mat(m,vector<long long>(m)); //initialize a matrix of m X m
+     
+        //input matrix
+        for(int i=0;i<m;i++)
+            for(int j=0;j<m;j++)
+                cin>>mat[i][j];
+ 
+        matrix ans = identity_matrix(m); //initialize answer with unit matrix of size m X m
+ 
+        //logarithmic expontiation
+        while(n!=0){
+            if(n%2==1)
+                    ans=mul(ans,mat);
+ 
+            mat = mul(mat,mat);
+            n=n/2;
+        }
+        
+        //output
+        for(int i=0;i<m;i++){
+            for(int j=0;j<m;j++)
+                cout<<ans[i][j]<<" ";
+            cout<<endl;
+        }
+ 
+    }
+}
+ 
+ 
+
+matrix mul(const matrix &a,const matrix &b){ //matrix multiplication function
     int r=a.size();
     int c=b[0].size();
     int k=b.size();
@@ -21,7 +64,7 @@ matrix mul(matrix a,matrix b){
     return ans;
 }
  
-matrix add(matrix a,matrix b){
+matrix add(const matrix &a,const matrix &b){ //matrix addition function
     int n=a.size();
     matrix ans(n,vector<long long>(n));
     for(int i=0;i<n;i++)
@@ -31,7 +74,7 @@ matrix add(matrix a,matrix b){
 }
  
  
-matrix sub(matrix a,matrix b){
+matrix sub(const matrix &a,const matrix &b){ //matrix subtraction function
     int n=a.size();
     matrix ans(n,vector<long long>(n));
     for(int i=0;i<n;i++)
@@ -41,43 +84,12 @@ matrix sub(matrix a,matrix b){
     return ans;
 }
  
+matrix identity_matrix(long long size){
  
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        long long m,n;
-        cin>>m>>n;
+    matrix ans(size,vector<long long>(size));
  
-        matrix mat(m,vector<long long>(m));
-        for(int i=0;i<m;i++)
-            for(int j=0;j<m;j++)
-                cin>>mat[i][j];
- 
-        matrix ans;
-        bool flag=false;
- 
-        while(n!=0){
-            if(n%2==1)
-                if(flag==false){
-                    ans=mat;
-                    flag=true;
-                }
-                else
-                    ans=mul(ans,mat);
- 
-            mat=mul(mat,mat);
-            n=n/2;
-        }
- 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<m;j++)
-                cout<<ans[i][j]<<" ";
-            cout<<endl;
-        }
- 
-    }
+    for(int i=0;i<size;i++){
+         ans[i][i]=1;
+    } 
+    return ans;
 }
- 
- 
- 
