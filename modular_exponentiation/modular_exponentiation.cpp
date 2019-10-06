e20ee94b9906bc52b4e15c577216cfd89a0d4168
Navkrishna <navkrishna.raghav@gmail.com>
2019-10-07 03:43:31 +0530

    Update modular_exponentiation.cpp

--- modular_exponentiation/modular_exponentiation.cpp
+++ modular_exponentiation/modular_exponentiation.cpp
@@ -1,24 +1,12 @@
-//recursive
-long long binpow(long long a, long long b) {
-    if (b == 0)
-        return 1;
-    long long res = binpow(a, b / 2);
-    if (b % 2)
-        return res * res * a;
-    else
-        return res * res;
-}
-
-
-//iterative
+//iterative modular exponentiation
 template <typename T>
 T modpow(T base, T exp, T modulus) {
   base %= modulus;
   T result = 1;
   while (exp > 0) {
-    if (exp & 1) result = (result * base) % modulus;
-    base = (base * base) % modulus;
-    exp >>= 1;
+    if (exp & 1) result = (result * base) % modulus; //if the bit of exponent is set, multiply the base to the result
+    base = (base * base) % modulus; //square the base
+    exp >>= 1;  //divide the exponent by 2
   }
   return result;
 }
